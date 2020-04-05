14a362a4da15e402f011a1ba4c318136a36dacc8
宇都宮靖之 <csya18003@g.nihon-u.ac.jp>
2020-04-05 05:57:36 -0700

    viewの理解

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+# たくさんのプロジェクトがある時の対処法
+app_name = 'polls'
+
 # ここではpathを通すと言う意味
 # 「同じようなデザインで内容が変わるページ（動的ページ）」を扱うのに適した仕組み
 
@@ -11,15 +14,18 @@ urlpatterns = [  # urlのパターンの入力
 
     #どうにか反映させる方法はないのかなぁ
     # path('<int:id>/', views.index, name = 'index'),
-    # path('<int:id>/', views.unko, name = 'unko'),
 
-    # path('<int;id>', views.index, name='index'),
+    path('<int;id>', views.index, name='index'),
+
+    # path('', views.unko, name = 'unko'),
     # path('', views.unko, name = 'unko')
     # path('<int:question_id>/', views.detail, name = 'detail'),
     # path('<int:question_id>/', views.result, name = 'result'),
     # path('<int:question_id>/', views.vote, name = 'vote')
 
     path('<int:question_id>/', views.detail, name='detail'),
+    # urlをpolls/specifics/12/ のように変更したいときには以下のようにする
+    path('specifics/<int:question_id>/', views.detail, name='detail'),
     # ex: /polls/5/results/
     path('<int:question_id>/result/', views.result, name='result'),
     # ex: /polls/5/vote/
--- polls/views.py
+++ polls/views.py
@@ -2,25 +2,60 @@
 from django.shortcuts import render
 
 # Create your views here.
-# from django.http.response import HttpResponse
+from django.http.response import HttpResponse
 
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 from django.urls import reverse
 
+# from django.template import loader
+
+from .models import Question
+
+# from django.http import Http404
+
+from django.shortcuts import get_object_or_404, render
+
+
+
 
 def kumasan(request):  # ここで書いているrequestは何？
-    return HttpResponse("いつもお疲れさまです")
+    # return HttpResponse("いつもお疲れさまです")
+    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # template = loader.get_template('polls/index.html')
+    # content = {
+    #     'latest_question_list':latest_question_list,
+    # }
+    # return HttpResponse(template.render(content, request)) #引数で置いたリクエストが反映されている
+
+    #ショートカットを利用してみる
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    content = {'latest_question_list':latest_question_list}
+    return render(request, 'polls/index.html', content)
+
 
 def index(request, id):
-    return HttpResponse("最強くまさん" + index(id))
+    return HttpResponse("最強くまさん" + str(id))
 
-def unko(request):
-    urlName = reverse("unko") #上でリバースを使っている
-    return HttpResponse("ello, world. You're at the polls index.{0}")
+# def unko(request):
+#     urlName = reverse("unko") #上でリバースを使っている
+#     return HttpResponse("ello, world. You're at the polls index.{0}", format(urlName))
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    # return HttpResponse("You're looking at question %s." % question_id)
+    #質問詳細ビューの作成
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    # return render(request, 'polls/detail.html', {'question':question})
+
+
+    # get_object_or_404()を使って書き換える
+    # get() を実行し、オブジェクトが存在しない場合には Http404 を送出することは非常によく使われるイディオムです
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls.detail.html', {'question':question})
+
 
 def result(request, question_id):
     response = "You're looking at the results of question %s."
